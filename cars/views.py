30a1ed704660b71e66f849e97f6cc24a5f3f717a
CN88H <cheenou88@gmail.com>
2022-06-11 04:25:30 -0500

    Registered Car Model with Admin Site + Completed POST Functionality for Car + Updated Rest Framework Decimal Settings

--- cars/views.py
+++ cars/views.py
@@ -3,9 +3,24 @@
 # # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CarSerializer
+from .models import Car
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def cars_list(request):
 
+    if request.method == "GET":
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
 
-    return Response('ok')
+    elif request.method == 'POST':
+        serializer = CarSerializer(data=request.data)
+        #the below code is like a short cut to the if statement
+        serializer.is_valid(raise_exception=True)
+        # if serializer.is_valid() == True:
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # else:
+        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
